9c66c39df900bae0d60014f8a33fedd50bc4a611
evgzabozhan <evgzabozhan@gmail.com>
2020-11-21 18:28:10 +0300

    Update 0.10 - Add user id in goal database

--- src/main/java/com/evgzabozhan/GoatGoal/config/WebSecurityConfig.java
+++ src/main/java/com/evgzabozhan/GoatGoal/config/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/login","/registration").permitAll()
                 .anyRequest().authenticated()
